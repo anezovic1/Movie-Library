aed108761ab100ecff7ed125de70989a30248b02
Anida Nezovic <anezovic1@etf.unsa.ba>
2022-12-16 11:14:42 +0100

    getById implemented

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -1,4 +1,36 @@
 package ba.unsa.etf.rpr.dao;
 
-public class AbstractDao {
+import ba.unsa.etf.rpr.domain.Idable;
+import ba.unsa.etf.rpr.exceptions.MovieException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public abstract class AbstractDao<T extends Idable> implements Dao<T> {
+    private Connection connecetion;
+    private String tableName;
+
+    public abstract T row2object(ResultSet rs) throws MovieException;
+    public T getById(int id) throws MovieException {
+        String query = "SELECT * FROM " + this.tableName + " WHERE id = ?";
+
+        try {
+            PreparedStatement stmt = this.connecetion.prepareStatement(query);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()) {
+                T result = row2object(rs);
+                rs.close();
+                return result;
+            }
+            else {
+                throw new MovieException("Object not found");
+            }
+        } catch (SQLException e) {
+            throw new MovieException(e.getMessage(), e);
+        }
+    }
+
 }
