890df4fa1e712310b6f4af5a08d4fe57e9ecea06
Anida Nezovic <anezovic1@etf.unsa.ba>
2022-12-21 19:42:03 +0100

    DaoFactory is added

---
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.domain.Watchlist;
+
+/**
+ * Factory method for singleton implementation od DAOs
+ * @author anida
+ */
+public class DaoFactory {
+    public DaoFactory() {
+    }
+
+    private static final MovieDao movieDao = new MovieDaoSQLImpl();
+    private static final WatchlistDao watchlistDao = new WatchlistDaoSQLImpl();
+    private static final UserDao userDao = new UserDaoSQLImpl();
+
+    public static MovieDao movieDao() {
+        return movieDao;
+    }
+
+    public static WatchlistDao watchlistDao() {
+        return watchlistDao;
+    }
+
+    public static UserDao userDao() {
+        return userDao;
+    }
+}
