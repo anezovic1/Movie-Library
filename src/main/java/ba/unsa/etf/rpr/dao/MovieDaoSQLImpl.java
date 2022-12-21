45fdd07d5ed49bdd5df80cef38bc61456fa9e887
Anida Nezovic <anezovic1@etf.unsa.ba>
2022-12-21 19:15:20 +0100

    implementation of MovieDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/MovieDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/MovieDaoSQLImpl.java
@@ -3,12 +3,47 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Movie;
 import ba.unsa.etf.rpr.exceptions.MovieException;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * MySQL Implementation of DAO
  * @author anida
  */
-public class MovieDaoSQLImpl  {
+public class MovieDaoSQLImpl extends AbstractDao<Movie> implements MovieDao {
 
+    public MovieDaoSQLImpl() {
+        super("movies");
+    }
+
+    @Override
+    public Movie row2object(ResultSet rs) throws MovieException {
+        try {
+            Movie movie = new Movie();
+            movie.setId(rs.getInt("id"));
+            movie.setName(rs.getString("name"));
+            movie.setDuration(rs.getString("duration"));
+            movie.setGenre(rs.getString("genre"));
+            movie.setRating(rs.getDouble("rating"));
+            movie.setSynopsis(rs.getString("synopsis"));
+            return movie;
+        } catch(SQLException e) {
+            throw new MovieException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> object2row(Movie object) {
+        Map<String, Object> item = new TreeMap<>();
+        item.put("id", object.getId());
+        item.put("name", object.getName());
+        item.put("duration", object.getDuration());
+        item.put("rating", object.getRating());
+        item.put("genre", object.getGenre());
+        item.put("synopsis", object.getSynopsis());
+        return item;
+    }
 }
