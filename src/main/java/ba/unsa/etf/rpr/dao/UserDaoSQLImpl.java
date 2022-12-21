5e12ca80b0a4aa2e1b2fd862e3c811aa3c45c518
Anida Nezovic <anezovic1@etf.unsa.ba>
2022-12-21 19:26:08 +0100

    implementation of UserDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
@@ -1,4 +1,47 @@
 package ba.unsa.etf.rpr.dao;
 
-public class UserDaoSQLImpl {
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.MovieException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * MySQL Implementation of DAO
+ * @author anida
+ */
+public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
+    public UserDaoSQLImpl() {
+        super("users");
+    }
+
+    @Override
+    public User row2object(ResultSet rs) throws MovieException {
+        try {
+            User user = new User();
+            user.setId(rs.getInt("id"));
+            user.setName(rs.getString("name"));
+            user.setLastName(rs.getString("lastName"));
+            user.setEmail(rs.getString("email"));
+            user.setPassword(rs.getString("password"));
+            user.setUsername(rs.getString("username"));
+            return user;
+        } catch(SQLException e) {
+            throw new MovieException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> object2row(User object) {
+        Map<String, Object> row = new TreeMap<>();
+        row.put("id", object.getId());
+        row.put("name", object.getName());
+        row.put("lastName", object.getLastName());
+        row.put("email", object.getEmail());
+        row.put("password", object.getPassword());
+        row.put("username", object.getUsername());
+        return row;
+    }
 }
