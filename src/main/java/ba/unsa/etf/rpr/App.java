475154a94330dabd10cdb26ca825cffe80c6987a
Anida Nezovic <anezovic1@etf.unsa.ba>
2022-12-25 01:40:05 +0100

    started working on Home Page

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,22 +1,20 @@
 package ba.unsa.etf.rpr;
 
-import ba.unsa.etf.rpr.dao.WatchlistDao;
-import ba.unsa.etf.rpr.dao.WatchlistDaoSQLImpl;
-import ba.unsa.etf.rpr.domain.Watchlist;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.List;
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/home.fxml"));
         primaryStage.setTitle("Hello, World!");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
     public static void main(String[] args) {
