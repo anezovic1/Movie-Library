2645a21b9a49c0ba9ec9febece64e81016dada12
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-01-02 14:43:28 +0100

    show Login page when login button is clicked

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -10,7 +10,7 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class App extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/home.fxml"));
         primaryStage.setTitle("Hello, World!");
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
--- src/main/java/ba/unsa/etf/rpr/controllers/LogInController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LogInController.java
@@ -1,6 +1,9 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -8,6 +11,23 @@ public class LogInController {
     public TextField fieldUsername;
     public PasswordField fieldPassword;
 
+    @FXML
+    public void initialize() {
+        fieldUsername.getStyleClass().removeAll("fieldNotCorrect");
+        fieldUsername.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                if(fieldUsername.getText().trim().isEmpty()) {
+                    fieldUsername.getStyleClass().removeAll("fieldCorrect");
+                    fieldUsername.getStyleClass().add("fieldNotCorrect");
+                }
+                else {
+                    fieldUsername.getStyleClass().removeAll("fieldNotCorrect");
+                    fieldUsername.getStyleClass().add("fieldCorrect");
+                }
+            }
+        });
+    }
     public void loginButtonClick(ActionEvent actionEvent) {
 
     }
