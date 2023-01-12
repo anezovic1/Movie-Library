d1caa37b0184545e9cc9b9b7121052ccee6de075
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-01-12 20:50:06 +0100

    click for log in and sign up

--- src/main/java/ba/unsa/etf/rpr/controllers/MovieController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MovieController.java
@@ -1,13 +1,40 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.domain.Movie;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class MovieController {
     public Label idSearchLabel;
     public TableView moviesTable;
     public TableColumn<Movie, String> genreColumn;
     public TableColumn<Movie, String> titleColumn;
+
+    public void loginButtonClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        stage.setTitle("Log in");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void signUpButtonClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/signup.fxml"));
+        stage.setTitle("Sign up");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
 }
