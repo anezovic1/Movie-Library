b126f05fa74cf3224f03bda90e9c2ade3ba326de
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-01-12 18:35:29 +0100

    show results for searched movie

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
@@ -12,6 +13,8 @@ import java.io.IOException;
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class HomeController {
+
+    public TextField idSearchItem;
 
     public void loginButtonClick(ActionEvent actionEvent) throws IOException {
         Stage stage = new Stage();
@@ -26,6 +29,18 @@ public class HomeController {
         Stage stage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/signup.fxml"));
         stage.setTitle("Sign up");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void searchBtnClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/movie.fxml"));
+        Parent root = loader.load();
+        MovieController movieController = loader.getController();
+        movieController.idSearchLabel.setText(movieController.idSearchLabel.getText() + " \"" + idSearchItem.getText() + "\"");
+        stage.setTitle("Movies");
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         stage.setResizable(false);
         stage.show();
---
+++ src/main/java/ba/unsa/etf/rpr/controllers/MovieController.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.scene.control.Label;
+
+public class MovieController {
+    public Label idSearchLabel;
+}
