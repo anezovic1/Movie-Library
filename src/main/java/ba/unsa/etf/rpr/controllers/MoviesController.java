bd732d38501f685ff3c16556856e3d4f98d1de02
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-02-04 14:20:16 +0100

    cancel button in movies.fxml

--- src/main/java/ba/unsa/etf/rpr/controllers/MoviesController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MoviesController.java
@@ -1,4 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 public class MoviesController {
+
+    public Button idCancelBtn;
+    public VBox idVbox;
+
+    public void cancelClick(ActionEvent actionEvent) {
+        Stage stage = (Stage)idCancelBtn.getScene().getWindow();
+        stage.close();
+    }
 }
