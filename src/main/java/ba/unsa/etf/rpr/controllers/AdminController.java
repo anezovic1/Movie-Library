14ca5d97d0054ed745d2315dbdf66e75cb401c59
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-02-16 00:44:53 +0100

    buttons for login and cancel

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
@@ -1,4 +1,34 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class AdminController {
+    public Button loginBtn;
+    public Button cancelBtn;
+    public TextField fieldUsername;
+    public TextField fieldPassword;
+
+    public void cancelClick(ActionEvent actionEvent) {
+        Stage stage = (Stage)cancelBtn.getScene().getWindow();
+        stage.close();
+    }
+
+    public void loginClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/admin_login.fxml"));
+        stage.setTitle("Log in");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
 }
