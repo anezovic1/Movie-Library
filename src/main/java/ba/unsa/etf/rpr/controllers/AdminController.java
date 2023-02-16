ab638f49746dd69f7935432151584639ce723d69
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-02-16 12:02:36 +0100

    action for admin login button

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -16,7 +18,9 @@ public class AdminController {
     public Button loginBtn;
     public Button cancelBtn;
     public TextField fieldUsername;
-    public TextField fieldPassword;
+
+    public Label adminNameLabel;
+    public PasswordField fieldPassword;
 
     public void cancelClick(ActionEvent actionEvent) {
         Stage stage = (Stage)cancelBtn.getScene().getWindow();
@@ -25,8 +29,14 @@ public class AdminController {
 
     public void loginClick(ActionEvent actionEvent) throws IOException {
         Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/admin_login.fxml"));
-        stage.setTitle("Log in");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/admin_login.fxml"));
+        Parent root = loader.load();
+
+        AdminController adminController = loader.getController();
+        adminController.adminNameLabel.setText(fieldUsername.getText());
+
+        adminNameLabel.setText(fieldUsername.getText());
+        stage.setTitle("Admin");
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         stage.setResizable(false);
         stage.show();
