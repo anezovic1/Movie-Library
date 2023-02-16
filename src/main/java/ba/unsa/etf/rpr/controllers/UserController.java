e0cea065e2e0a7b274d4f7d4fc4266268d4fea42
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-02-16 21:11:16 +0100

    list of all movies user can add

--- src/main/java/ba/unsa/etf/rpr/controllers/UserController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserController.java
@@ -1,15 +1,22 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.MovieManager;
+import ba.unsa.etf.rpr.domain.Movie;
+import ba.unsa.etf.rpr.exceptions.MovieException;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
@@ -18,15 +25,33 @@ public class UserController {
     public Button idLogoutBtn;
     public Button cancelBtn;
 
+
+    private final MovieManager movieManager = new MovieManager();
+    public ListView listView;
+
     public void logoutBtnClick(ActionEvent actionEvent) {
         Stage stage = (Stage)idLogoutBtn.getScene().getWindow();
         stage.close();
     }
 
-    public void createWatchlistClick(ActionEvent actionEvent) throws IOException {
+    public void createWatchlistClick(ActionEvent actionEvent) throws IOException, MovieException {
         Stage stage = new Stage();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/create_watchlist.fxml"));
         Parent root = loader.load();
+
+        UserController userController = loader.getController();
+
+        userController.listView.getItems().clear();
+
+        List<Movie> allMovies = FXCollections.observableList(movieManager.getAll());
+        List<String> namesOfAllMovies = new ArrayList<>();
+
+        for(int i = 0; i < allMovies.size(); i++) {
+            namesOfAllMovies.add(allMovies.get(i).getId() + ". " + allMovies.get(i).getName());
+        }
+
+        userController.listView.getItems().addAll(namesOfAllMovies);
+
         stage.setTitle("Creating watchlist");
         stage.getIcons().add(new Image("/img/footer.png"));
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
