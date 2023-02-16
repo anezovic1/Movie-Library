26a70dab95afef1a38dcef695d2dbbd01d8009ab
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-02-16 20:16:35 +0100

    GUI for watchlist

--- src/main/java/ba/unsa/etf/rpr/controllers/UserController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserController.java
@@ -1,19 +1,44 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class UserController {
     public Label userNameLabel;
     public Button idLogoutBtn;
+    public Button cancelBtn;
 
     public void logoutBtnClick(ActionEvent actionEvent) {
         Stage stage = (Stage)idLogoutBtn.getScene().getWindow();
         stage.close();
     }
 
-    public void createWatchlistClick(ActionEvent actionEvent) {
+    public void createWatchlistClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/create_watchlist.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("Creating watchlist");
+        stage.getIcons().add(new Image("/img/footer.png"));
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void createBtnClick(ActionEvent actionEvent) {
+    }
+
+    public void cancelBtnClick(ActionEvent actionEvent) {
+        Stage stage = (Stage)cancelBtn.getScene().getWindow();
+        stage.close();
     }
 }
