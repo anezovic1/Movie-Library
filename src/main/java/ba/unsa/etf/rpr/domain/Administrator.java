550c4d9e6f430a37fdee1b5c01450fe3917e0fd5
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-02-19 17:58:24 +0100

    small change

--- src/main/java/ba/unsa/etf/rpr/domain/Administrator.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Administrator.java
@@ -3,6 +3,7 @@ package ba.unsa.etf.rpr.domain;
 import java.util.Objects;
 
 public class Administrator implements Idable {
+
     private int id;
     private String username;
     private String password;
