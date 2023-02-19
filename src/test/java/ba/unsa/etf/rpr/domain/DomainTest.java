2ae4c5c0b6bcf45c78da645ae6665c5efe083125
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-02-19 18:58:42 +0100

    unit test - setter and getter in Administrator

--- src/test/java/ba/unsa/etf/rpr/domain/DomainTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/DomainTest.java
@@ -14,12 +14,24 @@ public class DomainTest {
         user.setLastName("Nekic");
         user.setEmail("neko@gmail.com");
         user.setPassword("nekooo");
+        user.setUsername("neko123");
 
         assertEquals(2, user.getId());
         assertEquals("Neko", user.getName());
         assertEquals("Nekic", user.getLastName());
         assertEquals("neko@gmail.com", user.getEmail());
         assertEquals("nekooo", user.getPassword());
+        assertEquals("neko123", user.getUsername());
+    }
+
+    @Test
+    public void testAdminSetterGetter() {
+        Administrator admin = new Administrator();
+        admin.setUsername("anida");
+        admin.setPassword("RPR2023");
+
+        assertEquals("RPR2023", admin.getPassword());
+        assertEquals("anida", admin.getUsername());
     }
 
 }
