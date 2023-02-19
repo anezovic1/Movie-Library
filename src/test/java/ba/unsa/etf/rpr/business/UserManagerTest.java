cb707910a5ac11679c9ce34848e940d7c6257bdc
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-02-19 17:59:57 +0100

    start of the unit testing

---
+++ src/test/java/ba/unsa/etf/rpr/business/UserManagerTest.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.business;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+public class UserManagerTest {
+
+}
