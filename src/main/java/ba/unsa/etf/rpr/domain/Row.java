3691b5f0aea43d82439c08d4199b29115415a352
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-02-20 22:14:32 +0100

    javadoc for Row done

--- src/main/java/ba/unsa/etf/rpr/domain/Row.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Row.java
@@ -2,27 +2,54 @@ package ba.unsa.etf.rpr.domain;
 
 import javafx.beans.property.SimpleStringProperty;
 
+/**
+ * Instead of directly adding the hashmap to the table, it is easier and more convenient to create a class.
+ * This class can provide that.
+ *
+ * @author Anida Nezovic
+ */
 public class Row {
     private final SimpleStringProperty header = new SimpleStringProperty("");
     private final SimpleStringProperty value = new SimpleStringProperty("");
 
+    /**
+     * Class constructor.
+     */
     public Row() {
         this("","");
     }
 
+    /**
+     * Class constructor specifying header and value.
+     */
     public Row(String header, String value) {
         setHeader(header);
         setValue(value);
     }
 
+    /**
+     * Getter for header
+     *
+     * @return String
+     */
     public String getHeader() {
         return header.get();
     }
 
+    /**
+     * Setter for the header
+     *
+     * @param header the header to set
+     */
     public void setHeader(String header) {
         this.header.set(header);
     }
 
+    /**
+     * Setter for the value
+     *
+     * @param value the value to set
+     */
     public void setValue(String value) {
         this.value.set(value);
     }
@@ -31,6 +58,11 @@ public class Row {
         return header;
     }
 
+    /**
+     * Getter for value
+     *
+     * @return String
+     */
     public String getValue() {
         return value.get();
     }
