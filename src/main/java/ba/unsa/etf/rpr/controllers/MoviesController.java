55d8f5347b21627467a1c953bd7c59bf26e82b61
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-02-20 23:06:59 +0100

    javadoc for MoviesController.java

--- src/main/java/ba/unsa/etf/rpr/controllers/MoviesController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MoviesController.java
@@ -6,12 +6,22 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+/**
+ * JavaFX controller for full movie list management
+ *
+ * @author Anida Nezovic
+ */
 public class MoviesController {
 
     public Button idCancelBtn;
     public VBox idVbox;
     public ScrollPane idScrollPane;
 
+    /**
+     * Method that closes current window when cancel button is clicked.
+     *
+     * @param actionEvent
+     */
     public void cancelClick(ActionEvent actionEvent) {
         Stage stage = (Stage)idCancelBtn.getScene().getWindow();
         stage.close();
