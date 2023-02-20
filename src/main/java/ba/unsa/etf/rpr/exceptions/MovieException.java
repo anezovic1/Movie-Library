b31d6967213a9ac3add0a82ebbecc25f4a9c1818
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-02-21 00:27:38 +0100

    javadoc for exception

--- src/main/java/ba/unsa/etf/rpr/exceptions/MovieException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/MovieException.java
@@ -1,5 +1,10 @@
 package ba.unsa.etf.rpr.exceptions;
 
+/**
+ * A Class that represents use-defined exception
+ *
+ * @author Anida Nezovic
+ */
 public class MovieException extends Exception {
     public MovieException(String msg, Exception e) {
         super(msg, e);
