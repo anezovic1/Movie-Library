ee94b8c66d6cf750e09981aea5390e19109e6629
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-02-20 15:24:27 +0100

    unit test - add a new movie

--- src/test/java/ba/unsa/etf/rpr/business/MovieManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/MovieManagerTest.java
@@ -1,4 +1,24 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.domain.Movie;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.MovieException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class MovieManagerTest {
+    private MovieManager movieManager = new MovieManager();
+    private Movie newMovie = new Movie("Inception", "action", "2h 38m", "8.8");
+
+    @BeforeEach
+    public void initializeObjectsWeNeed() {
+        movieManager = Mockito.mock(MovieManager.class);
+    }
+
+    @Test
+    void addNewMovie() throws MovieException {
+        movieManager.add(newMovie);
+        Mockito.verify(movieManager).add(newMovie);
+    }
 }
