2fda770d5a5dc0872a9256d8c77659af47074619
Anida Nezovic <anezovic1@etf.unsa.ba>
2023-02-20 20:38:14 +0100

    javadoc for Administrator done

--- src/main/java/ba/unsa/etf/rpr/domain/Administrator.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Administrator.java
@@ -28,7 +28,7 @@ public class Administrator implements Idable {
     }
 
     /**
-     * Sets the id. At the moment we don't need this method
+     * Setter for the id. At the moment we don't need this method
      * because id is autoincremented.
      *
      * @param id the id to set
@@ -38,27 +38,57 @@ public class Administrator implements Idable {
 
     }
 
+    /**
+     * Getter for Id
+     *
+     * @return int
+     */
     @Override
     public int getId() {
         return 0;
     }
 
+    /**
+     * Getter for username
+     *
+     * @return String
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Setter for username
+     *
+     * @param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Getter for password
+     *
+     * @return String
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Setter for password
+     *
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Method used for printing admins id, username and password.
+     *
+     * @return String
+     */
     @Override
     public String toString() {
         return "Administrator{" +
@@ -68,6 +98,11 @@ public class Administrator implements Idable {
                 '}';
     }
 
+    /**
+     * Method used for comparing Administrator instances.
+     *
+     * @return boolean
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
